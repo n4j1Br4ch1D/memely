125155837dad9fe914a6c597422b70a9eb2d2d81
Najib-Rachid <kawner93@gmail.com>
2022-08-08 15:35:18 +0200

    add fetch tenor api

--- src/app/searcher/searcher.component.ts
+++ src/app/searcher/searcher.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { fromFetch } from 'rxjs/fetch';
+import { switchMap, of, catchError } from 'rxjs';
 @Component({
   selector: 'app-searcher',
   templateUrl: './searcher.component.html',
@@ -10,6 +11,35 @@ export class SearcherComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+   const apiKey$ = 'AIzaSyA0v010oX8a0ApcRYmAeN-omDVGDitxPT8';
+   const pageLimit$ = 1;
+
+  const data$ = fromFetch(`https://tenor.googleapis.com/v2/search?q=excited&key=${apiKey$}&limit=${pageLimit$}`).pipe(
+  switchMap(response => {
+    if (response.ok) {
+      // OK return data
+      return response.json();
+    } else {
+      // Server is returning a status requiring the client to try something else.
+      return of({ error: true, message: `Error ${ response.status }` });
+    }
+  }),
+  catchError(err => {
+    // Network or other error, handle appropriately
+    console.error(err);
+    return of({ error: true, message: err.message })
+  })
+);
+
+data$.subscribe({
+  next: result => console.log(result),
+  complete: () => console.log('done')
+});
   }
 
 }
+
+
+
+
