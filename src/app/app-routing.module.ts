f7955831571ebe9deebd8f2a3f244d58c17fd1b7
Najib-Rachid <kawner93@gmail.com>
2022-08-08 16:14:41 +0200

    fix home url routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent }
 ];
 
