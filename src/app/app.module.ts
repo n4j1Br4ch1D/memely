16b996dd7371aed1c139f0f2d4e8988cc1d7533b
Najib-Rachid <kawner93@gmail.com>
2022-08-08 13:16:56 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearcherComponent } from './searcher/searcher.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearcherComponent,
+    ViewerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
