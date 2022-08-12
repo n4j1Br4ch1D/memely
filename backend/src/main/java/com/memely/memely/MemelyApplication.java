a0a652f373d45d21d3c974b2ad694e30b3a78124
Najib-Rachid <kawner93@gmail.com>
2022-08-12 15:06:18 +0200

    add backend spring boot

---
+++ backend/src/main/java/com/memely/memely/MemelyApplication.java
@@ -0,0 +1,13 @@
+package com.memely.memely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemelyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/memely/memely/MemelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memely.memely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
