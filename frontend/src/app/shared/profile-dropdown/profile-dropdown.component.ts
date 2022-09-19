018307c2cd526f8382b33edd8fa498f5839c72aa
Najib-Rachid <kawner93@gmail.com>
2022-09-19 23:41:20 +0200

    add witch betwwen profile dropdown and signin/up btns

--- frontend/src/app/shared/profile-dropdown/profile-dropdown.component.ts
+++ frontend/src/app/shared/profile-dropdown/profile-dropdown.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileDropdownComponent implements OnInit {
 
+  signedIn:boolean= false;
   constructor() { }
 
   ngOnInit(): void {
