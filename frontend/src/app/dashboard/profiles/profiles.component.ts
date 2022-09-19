250e646e11724cb51442cae3d343782c04ac4b1d
Najib-Rachid <kawner93@gmail.com>
2022-09-19 22:57:43 +0200

    add Form new & edit collabsible profiles table 1st attempt

--- frontend/src/app/dashboard/profiles/profiles.component.ts
+++ frontend/src/app/dashboard/profiles/profiles.component.ts
@@ -1,82 +1,115 @@
-import {DecimalPipe} from '@angular/common';
-import {Component, OnInit, Type, QueryList, ViewChildren, Input} from '@angular/core';
-import {Observable} from 'rxjs';
+import { DecimalPipe } from '@angular/common';
+import {
+  Component,
+  OnInit,
+  Type,
+  QueryList,
+  ViewChildren,
+  Input,
+} from '@angular/core';
+import { Observable } from 'rxjs';
 
-import {Profile} from './profile';
-import {ProfileService} from './profiles.service';
-import {NgbdSortableHeader, SortEvent} from './sortable.directive';
-import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { Profile } from './profile';
+import { ProfileService } from './profiles.service';
+import { NgbdSortableHeader, SortEvent } from './sortable.directive';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ngbd-modal-confirm-autofocus',
   template: `
-  <div class="modal-header">
-    <h4 class="modal-title" id="modal-title">Profile deletion</h4>
-    <button type="button" class="btn-close" aria-label="Close button" aria-describedby="modal-title" (click)="modal.dismiss('Cross click')"></button>
-  </div>
-  <div class="modal-body">
-    <p><strong>Are you sure you want to delete <span class="text-primary">{{ profile.name }}</span> profile?</strong></p>
-    <p>All information associated to this user profile will be permanently deleted.
-    <span class="text-danger">This operation can not be undone.</span>
-    </p>
-  </div>
-  <div class="modal-footer">
-    <button type="button" class="btn btn-outline-secondary" (click)="modal.dismiss('cancel click')">Cancel</button>
-    <button type="button" ngbAutofocus class="btn btn-danger" (click)="modal.close('Ok click')">I Confirm</button>
-  </div>
-  `
+    <div class="modal-header">
+      <h4 class="modal-title" id="modal-title">Profile deletion</h4>
+      <button
+        type="button"
+        class="btn-close"
+        aria-label="Close button"
+        aria-describedby="modal-title"
+        (click)="modal.dismiss('Cross click')"
+      ></button>
+    </div>
+    <div class="modal-body">
+      <p>
+        <strong
+          >Are you sure you want to delete
+          <span class="text-primary">{{ profile.name }}</span> profile?</strong
+        >
+      </p>
+      <p>
+        All information associated to this user profile will be permanently
+        deleted.
+        <span class="text-danger">This operation can not be undone.</span>
+      </p>
+    </div>
+    <div class="modal-footer">
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        (click)="modal.dismiss('cancel click')"
+      >
+        Cancel
+      </button>
+      <button
+        type="button"
+        ngbAutofocus
+        class="btn btn-danger"
+        (click)="modal.close('Ok click')"
+      >
+        I Confirm
+      </button>
+    </div>
+  `,
 })
 export class NgbdModalConfirmAutofocus implements OnInit {
-
   @Input() profile!: Profile;
 
   constructor(public modal: NgbActiveModal) {}
 
-  ngOnInit() {    
-  }
-
+  ngOnInit() {}
 }
 
-const MODALS: {[name: string]: Type<any>} = {
-  autofocus: NgbdModalConfirmAutofocus
+const MODALS: { [name: string]: Type<any> } = {
+  autofocus: NgbdModalConfirmAutofocus,
 };
 
-
-  @Component({
-    selector: 'app-profiles',
-    templateUrl: './profiles.component.html',
-    styleUrls: ['./profiles.component.scss'],
-    providers: [ProfileService, DecimalPipe]
-    
-  })
-
-  export class ProfilesComponent implements OnInit {
+@Component({
+  selector: 'app-profiles',
+  templateUrl: './profiles.component.html',
+  styleUrls: ['./profiles.component.scss'],
+  providers: [ProfileService, DecimalPipe],
+})
+export class ProfilesComponent implements OnInit {
   profiles$: Observable<Profile[]>;
+  profile:Profile|null= null;
   total$: Observable<number>;
 
   @ViewChildren(NgbdSortableHeader)
-    headers!: QueryList<NgbdSortableHeader>;
+  headers!: QueryList<NgbdSortableHeader>;
 
   constructor(public service: ProfileService, private _modalService: NgbModal) {
     this.profiles$ = service.profiles$;
     this.total$ = service.total$;
   }
 
-  ngOnInit(): void {
-  }
-  
-  open(name: string, profile:Profile) { 
+  ngOnInit(): void {}
 
-const modalRef =  this._modalService.open(MODALS[name]);
-
-modalRef.componentInstance.profile = profile;
-
-  
+  delete(name: string, profile: Profile) {
+    const modalRef = this._modalService.open(MODALS[name]);
+    modalRef.componentInstance.profile = profile;
   }
 
-  onSort({column, direction}: SortEvent) {
+  form(name: string, profile: Profile | null) {
+  //  this.f.isCollapsed = false;
+  this.profile = profile;
+
+    // this.isCollapsed = this.isCollapsed;
+    console.log(11110);
+    
+    // modalRef.componentInstance.profile = profile;
+  }
+
+  onSort({ column, direction }: SortEvent) {
     // resetting other headers
-    this.headers.forEach(header => {
+    this.headers.forEach((header) => {
       if (header.sortable !== column) {
         header.direction = '';
       }
@@ -85,4 +118,6 @@ modalRef.componentInstance.profile = profile;
     this.service.sortColumn = column;
     this.service.sortDirection = direction;
   }
+
+  public isCollapsed: boolean = true;
 }
