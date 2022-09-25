7b2e0ed5be8869b85bc56822c47fbed07fb6442d
Najib-Rachid <kawner93@gmail.com>
2022-09-25 19:45:08 +0200

    integrate chat api

--- frontend/src/app/_services/chat.service.ts
+++ frontend/src/app/_services/chat.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  ResPath = environment.apiUrl+'messages';
+  getUserMessages(userId: Number, friendId: Number): Observable<Object> {
+    return this.http.get<Object>(`${this.ResPath}/user/${userId}/friend/${friendId}`);
+  }
+  getUserMessagesFriends(userId: Number): Observable<Object> {
+    return this.http.get<Object>(`${this.ResPath}/${userId}/friends`);
+  }
 }
--- frontend/src/app/profile/my-messages/my-messages.component.ts
+++ frontend/src/app/profile/my-messages/my-messages.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/_services/auth.service';
+import { ChatService } from 'src/app/_services/chat.service';
 
 @Component({
   selector: 'app-my-messages',
   templateUrl: './my-messages.component.html',
-  styleUrls: ['./my-messages.component.scss']
+  styleUrls: ['./my-messages.component.scss'],
 })
 export class MyMessagesComponent implements OnInit {
-
-  constructor() { }
-
+  userId: any;
+  friendId: any;
+  messages: any = [];
+  friends: any = [];
+  constructor(
+    private authService: AuthService,
+    private chatService: ChatService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-  }
 
+    this.userId = this.authService.getSignedInUser()['id'];
+    this.friendId = 2;
+    this.chatService
+      .getUserMessages(this.userId, this.friendId)
+      .subscribe((data) => {
+        this.messages = data;
+      });
+    this.chatService
+      .getUserMessagesFriends(this.userId)
+      .subscribe((data) => {
+        this.friends = data;
+      });
+  }
 }
