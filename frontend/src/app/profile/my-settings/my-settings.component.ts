2227708b393ae49213cb3acb2e92fa3e9f8ac3ac
Najib-Rachid <kawner93@gmail.com>
2022-09-25 20:02:22 +0200

    integrate my settings

--- frontend/src/app/profile/my-settings/my-settings.component.ts
+++ frontend/src/app/profile/my-settings/my-settings.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-my-settings',
   templateUrl: './my-settings.component.html',
-  styleUrls: ['./my-settings.component.scss']
+  styleUrls: ['./my-settings.component.scss'],
 })
 export class MySettingsComponent implements OnInit {
-
-  constructor() { }
+  authUser: any;
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  }
 
+    this.authUser = this.authService.getSignedInUser();
+    
+  }
 }
